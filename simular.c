e6ef588e840d2b6fab7760b236190a1a376a9c6a
CarrascoII <tomasbccarrasco@gmail.com>
2016-10-04 14:19:54 +0100

    Update simular.c

--- simular.c
+++ simular.c
@@ -1,25 +1,31 @@
 int simular(char *arg2) {
-  atrasar();
-  int numAnos, i = 0, j, newValue[NUM_CONTAS];
-  numAnos = atoi(*arg2);
+  int numAnos, i, j;
+  int newValue[NUM_CONTAS];
+  numAnos = atoi(arg2);
 
   if(numAnos <= 0)
     return -1;
 
-  if(numAnos == 0 && strcmp(agr2,ZERO) == 0)
-    return -1
+  if(numAnos == 0 && strcmp(arg2,ZERO) == 0)
+    return -1;
 
   for(j = 0; j < NUM_CONTAS; j++) {
     newValue[j] = contasSaldos[j];
-  }
-
+  }  
+  i = 0;
   while(i <= numAnos) {
-    printf("SIMULACAO: Ano %d", i);
-    for(j = 0, j < NUM_CONTAS, j++) {
+    atrasar();
+    printf("SIMULACAO: Ano %d\n", i);
+    printf("=================\n");
+    for(j = 0; j < NUM_CONTAS; j++) {
       int n = j + 1;
-      printf("Conta %d, Saldo %d", n, newValue[j]);
-      newValue[j] = max(newValue[j] * (1 + TAXAJURO) - CUSTOMANUTENCAO);
+      if(newValue[j] >= 0)
+        printf("Conta %d, Saldo %d\n", n, newValue[j]);
+      else
+        printf("Conta %d, Saldo 0\n", n);
+      newValue[j] = (newValue[j] * (1 + TAXAJURO) - CUSTOMANUTENCAO);
     }
+    printf("\n");
     i++;
   }
 
